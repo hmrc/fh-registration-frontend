da5ee3863a5309db8ba31c05965035a485672e2f
Cosmin Marian <cosmin.marian@digital.hmrc.gov.uk>
2017-10-03 16:18:50 +0100

    Corrected access to conf

--- app/uk/gov/hmrc/fhddsfrontend/controllers/ExternalUrls.scala
+++ app/uk/gov/hmrc/fhddsfrontend/controllers/ExternalUrls.scala
@@ -22,7 +22,7 @@ import javax.inject.{Inject, Singleton}
 class ExternalUrls @Inject() (ds: CommonPlayDependencies) {
 
   val businessCustomerVerificationUrl = ds.conf
-    .getString("microservice.services.business-customer-urls.business-verification")
+    .getString(s"${ds.env.mode}.microservice.services.business-customer-urls.business-verification")
     .getOrElse("http://localhost:9923/business-customer/FHDDS?backLinkUrl=http://localhost:1118/fhdds/continue")
 
 
