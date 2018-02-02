1d856986b32673b3a0a900fb7547520803ab510d
Ali <ali@nubz.com>
2018-02-02 14:39:27 +0000

    spikeRadioGroup: new TextInput component plus examples

--- app/uk/gov/hmrc/fhregistrationfrontend/views/helpers/YesNoTextParams.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/views/helpers/YesNoTextParams.scala
@@ -18,14 +18,31 @@ package uk.gov.hmrc.fhregistrationfrontend.views.helpers
 
 /**
   * Created by ali on 01/02/18.
+  *
+  * for the YesNoText component, where a target value is optional
+  * we can ask if the value is to be supplied using Yes/No radio buttons
+  * these params are required to use the component:
+  *
+  * @param name will be the name of that target value
+  * @param context is optional and will separate many components of
+  *                the same name in the same form e.g. repeating sections
+  * @param yesNoLabel the label for the actual Yes/No question
+  * @param yesNoHintText any hint text for the Yes/No question
+  * @param valueLabel the label for the target value if Yes has been answered
+  * @param valueHintText any hint text for the revealed target value field
+  * @param targetForNo an optional field name to be required if No is answered
+  * @param noInputLabel label for field required if No is answered
+  * @param noInputHintText hint text for the field required if No is answered
+  * @param formHasErrors boolean to inform styling of component
   */
+
 case class YesNoTextParams (
                             name: String,
                             context: Option[String] = None,
-                            label: String,
-                            hintText: Option[String] = None,
-                            yesInputLabel: String,
-                            yesInputHintText: Option[String] = None,
+                            yesNoLabel: String,
+                            yesNoHintText: Option[String] = None,
+                            valueLabel: String,
+                            valueHintText: Option[String] = None,
                             targetForNo: Option[String] = None,
                             noInputLabel: Option[String] = None,
                             noInputHintText: Option[String] = None,
