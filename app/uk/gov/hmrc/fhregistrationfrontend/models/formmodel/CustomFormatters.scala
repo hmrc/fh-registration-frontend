2c7924d6cc6c61c1d12dd8f8b2a9315bc2aca331
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2018-02-08 15:50:50 +0000

    fixed a bug

--- app/uk/gov/hmrc/fhregistrationfrontend/models/formmodel/CustomFormatters.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/models/formmodel/CustomFormatters.scala
@@ -35,7 +35,7 @@ object CustomFormatters {
 
   def requiredRadioButton(requiredKey: String, condition: String): Formatter[Option[Boolean]] = new Formatter[Option[Boolean]] {
     override def bind(key: String, data: Map[String, String]): Either[Seq[FormError], Option[Boolean]] = {
-      if (data(requiredKey) == condition) {
+      if (data.getOrElse(requiredKey, "") == condition) {
         Right(data.getOrElse(key, "")).right.flatMap {
           case "true"  => Right(Some(true))
           case "false" => Right(Some(false))
