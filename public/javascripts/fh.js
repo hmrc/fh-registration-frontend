39862aeb0a7661c7bfb53826427b082314b0333e
Alistair Macdonald <ali@nubz.com>
2018-02-23 13:36:53 +0000

    FHDDS-769: progressive enhancement for print link and use messages for buttons

--- public/javascripts/fh.js
+++ public/javascripts/fh.js
@@ -219,6 +219,8 @@
 
 
 function init() {
+  // show js-enabled items
+  $('.js-show').show()
   // Use GOV.UK shim-links-with-button-role.js to trigger a link styled to look like a button,
   // with role="button" when the space key is pressed.
   GOVUK.shimLinksWithButtonRole.init()
