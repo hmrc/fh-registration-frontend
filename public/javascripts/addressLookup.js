f1370c6eb1228a46c7c4dff51576bf5cf7a9e87c
Alistair Macdonald <ali@nubz.com>
2018-04-05 16:41:38 +0100

    FHDDS-971: added pagination and filter to address lookup

--- public/javascripts/addressLookup.js
+++ public/javascripts/addressLookup.js
@@ -1,4 +1,22 @@
 (function($) {
+  function getParameterByName (name) {
+    var match = new RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search)
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '))
+  }
+
+  function pluralise (num, singular, plural) {
+    return num === 1 ? singular : plural
+  }
+
+  var Utils = {
+    getParameterByName: getParameterByName,
+    pluralise: pluralise
+  };
+  var Store = {};
+  var perPage = parseInt(Utils.getParameterByName('perPage'), 10) || 10;
+  var maxCount = parseInt(Utils.getParameterByName('maxCount'), 10) || 50;
+  var postcodeRegex = /(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/;
+  var lookUpPath = '/fhdds/address-lookup?postcode=';
   // initialise the display of js-only controls
   // the display will default to an open manual address
   // without a link to postcode lookup - if JS is enabled
@@ -8,7 +26,6 @@
   // because we can have multiple instances on the same page,
   // grab all postcode fields - if any have a value
   // open the parent manual container
-  // todo: aria labelling
   $('[name$="postcode"]').each(function () {
     var $this = $(this),
       $manual = $this.parents('.address-manual-container'),
@@ -24,18 +41,16 @@
     }
   });
 
-  var lookUpPath = '/fhdds/address-lookup?postcode=';
-  var postcodeRegex = /(([gG][iI][rR] {0,}0[aA]{2})|((([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y]?[0-9][0-9]?)|(([a-pr-uwyzA-PR-UWYZ][0-9][a-hjkstuwA-HJKSTUW])|([a-pr-uwyzA-PR-UWYZ][a-hk-yA-HK-Y][0-9][abehmnprv-yABEHMNPRV-Y]))) {0,}[0-9][abd-hjlnp-uw-zABD-HJLNP-UW-Z]{2}))$/;
-
   // address mapping is custom given the different fieldnames
   // in ETMP and DES schemas and the ADDRESS_LOOKUP response:
-  function populateAddress(address, context) {
+  function populateAddress (index, store) {
+    var address = store.addresses[index].address;
     $.each([1, 2, 3], function (i, lineNum) {
       var line = address.lines[i] || '';
-      $('[name="' + context + '.' + 'Line' + (lineNum)).val(line);
+      $('[name="' + store.context + '.' + 'Line' + (lineNum)).val(line);
     })
-    $('[name="' + context + '.' + 'Line4').val(address.town);
-    $('[name="' + context + '.' + 'postcode').val(address.postcode);
+    $('[name="' + store.context + '.' + 'Line4').val(address.town);
+    $('[name="' + store.context + '.' + 'postcode').val(address.postcode);
   }
 
   function clearAddressFields(context) {
@@ -44,13 +59,107 @@
     });
   }
 
-  function showError(error, $container) {
-    clearError($container);
-    $container.find('.lookup-results-fieldset').empty().before('<span class="error-message" role="alert">' + error + '</span>')
+  function showPage (page, store) {
+    var start = page * perPage
+    var end = start + perPage
+    var nextPage = store.count > end ? page + 1 : null
+    var prevPage = end - perPage > 0 ? page - 1 : null
+    // remove any previous pagination navigation
+    store.$results.siblings('.pagination').off().remove()
+    // build the pagination navigation
+    var paginationNav = ['<div class="pagination" style="margin-top:1em;position:relative;overflow:visible;">']
+    // we are using zero indexed paging
+    if (prevPage || prevPage === 0) {
+      paginationNav.push('<div style="float:left; width: 49%;"><a href="#" class="prev-page page-nav__link page-nav__link--previous"><span class="page-nav__label">Previous</span><span class="page-nav__title">' + perPage + ' addresses</span></a></div>')
+    }
+    if (nextPage) {
+      var remainder = end + perPage <= store.count ? perPage : store.count % end
+      paginationNav.push('<div style="float:right; width: 49%"><a href="#" class="next-page page-nav__link page-nav__link--next"><span class="page-nav__label">Next</span><span class="page-nav__title">' + remainder + ' ' + Utils.pluralise(remainder, 'address', 'addresses') + '</span></a></div>')
+    }
+    paginationNav.push('</div>')
+    // insert pagination navigation to DOM
+    $(paginationNav.join('')).insertAfter(store.$results)
+    $('.pagination')
+      .on('click', 'a.next-page', function (e) {
+        e.preventDefault()
+        showPage(nextPage, store)
+      })
+      .on('click', 'a.prev-page', function (e) {
+        e.preventDefault()
+        showPage(prevPage, store)
+      })
+
+    showResults(start, end, store)
   }
 
-  function clearError($container) {
-    $container.find('.error-message').remove()
+  function showError (error, store, $input) {
+    clearError(store)
+    $('<span class="error-message" role="alert">' + error + '</span>').insertBefore($input)
+    $input && $input.addClass('form-control-error').focus()
+    store.$results.empty().siblings('.pagination').remove()
+  }
+
+  function clearError (store) {
+    store
+      .$container
+      .find('.form-control-error')
+      .removeClass('form-control-error')
+      .end()
+      .find('.error-message')
+      .remove()
+  }
+
+  function initStore (context) {
+    var $container = $('#' + context + '-fieldset')
+    Store[context] = {
+      context: context,
+      count: 0,
+      addresses: [],
+      $container: $container,
+      $results: $('#' + context + '-results').off(),
+      $postcodeInput: $container.find('input.postcode-value'),
+      $filterInput: $container.find('input.property-value'),
+      legend: ''
+    };
+    return Store[context];
+  }
+
+  function buildRadios (start, end, store) {
+    var results = store.addresses.slice(start, end)
+    var i = start
+    return results.reduce(function (list, next) {
+      var address = next.address
+      list.push('<div class="multiple-choice"><input class="postcode-lookup-result" type="radio" id="' + store.context + '-result-' + i + '" name="' + store.context + '-result" value="' + i + '"><label for="' + store.context + '-result-' + i + '">');
+      list.push(address.lines.join(', '));
+      list.push(', ' + address.town + ', ');
+      list.push(address.postcode);
+      list.push('</label></div>');
+      i++
+      return list
+    }, [])
+  }
+
+  function processResults (data, store) {
+    store.count = data.addresses.length
+    store.addresses = data.addresses
+    store.legend = '<legend class="form-label-bold">' + store.count + ' ' + Utils.pluralise(store.count, 'address', 'addresses') + ' found:</legend>'
+
+    if (store.count > maxCount) {
+      showError('We found more than ' + maxCount + ' results for "' + store.$postcodeInput.val() + '", please enter a property name or number and try again or enter the address manually', store, store.$filterInput)
+      return false
+    }
+    showPage(0, store)
+  }
+
+  function showResults (start, end, store) {
+    var pagedResults = buildRadios(start, end, store);
+    store.$results
+      .html(store.legend + pagedResults.join(''))
+      .focus()
+      .on('click', '.postcode-lookup-result', function (e) {
+        var index = $(e.currentTarget).val();
+        populateAddress(index, store)
+      })
   }
 
   function showResult(data, context) {
@@ -80,22 +189,24 @@
       .find('.postcode-lookup-result:first').focus()
   }
 
-  function searchAddress(url, context) {
-    var $resultsEl = $('#' + context + '-results'),
-      $container = $resultsEl.parents('.address-lookup-container');
+  function searchAddress(url, store) {
     // remove previous results
-    $resultsEl.empty();
+    store.$results
+      .empty()
+      .siblings('.pagination')
+      .remove()
     // clear down previous address fields
-    clearAddressFields(context);
+    clearAddressFields(store.context);
     // clear down errors
-    clearError($container);
+    clearError(store);
 
     $.ajax({
       type: 'GET',
       url: url,
       dataType: "json",
       success: function(data) {
-        showResult(data, context);
+        //showResult(data, context);
+        processResults(data, store)
       },
       error: function(jqXHR) {
         //doError(jqXHR, context);
@@ -105,18 +216,28 @@
     });
   }
 
-  $('.address-lookup').on('click', function() {
-    var $container = $(this).parents('.address-lookup-container'),
-      postcode = $container.find('.postcode-value').val().replace(/\s/g,''),
-      url = lookUpPath + postcode,
-      context = CSS.escape($(this).data('context'));
-
-    if (!postcode.match(postcodeRegex)){
-      showError('The postcode you have searched with is not a valid UK postcode', $container);
-      return;
+  $('.address-lookup').on('click', function () {
+    var context = CSS.escape($(this).data('context'));
+    var url;
+    var store = initStore(context);
+    var propertyFilter = store.$filterInput.val();
+    var postcode = store.$postcodeInput.val().replace(/\s/g,'').toUpperCase();
+    if (postcode === '') {
+      showError('You must enter a UK postcode to look up an address', store, store.$postcodeInput)
+      return false
+    }
+    if (!postcode.match(postcodeRegex)) {
+      showError('The postcode you have searched with is not a valid UK postcode', store, store.$postcodeInput)
+      return false
     }
 
-    searchAddress(url, context);
+    url = lookUpPath + postcode;
+
+    if (propertyFilter !== '') {
+      url += '&filter=' + encodeURIComponent(propertyFilter);
+    }
+
+    searchAddress(url, store);
   });
 
   var manualMode = function (context) {
