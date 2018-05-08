13bde6df09d5ee09ff4ad14b8366a6e4abedcfac
Alistair Macdonald <ali@nubz.com>
2018-05-08 15:21:17 +0100

    FHDDS-1037: removed local test stubs from JourneyAction

--- app/uk/gov/hmrc/fhregistrationfrontend/actions/JourneyAction.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/actions/JourneyAction.scala
@@ -89,7 +89,7 @@ class JourneyAction (implicit val save4LaterService: Save4LaterService, errorHan
     val result: EitherT[Future, Result, JourneyRequest[A]] = for {
       cacheMap ← EitherT(loadCacheMap)
       _ ← checkAmendmentJourney(cacheMap).toEitherT[Future]
-      journeyPages ← getJourneyPages(cacheMap, r.email).toEitherT[Future]
+      journeyPages ← getJourneyPages(cacheMap).toEitherT[Future]
       journeyState = loadJourneyState(journeyPages, cacheMap)
       bpr ← findBpr(cacheMap).toEitherT[Future]
       bt ← getBusinessType(cacheMap).toEitherT[Future]
@@ -151,7 +151,7 @@ class JourneyAction (implicit val save4LaterService: Save4LaterService, errorHan
     }
   }
 
-  def getJourneyPages(cacheMap: CacheMap, ggMail: Option[String])(implicit request: Request[_]): Either[Result, JourneyPages] = {
+  def getJourneyPages(cacheMap: CacheMap)(implicit request: Request[_]): Either[Result, JourneyPages] = {
     getBusinessType(cacheMap).right flatMap {
       _ match {
         case BusinessType.CorporateBody ⇒ Right(Journeys.limitedCompanyPages)
