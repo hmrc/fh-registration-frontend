c4c3309b9cf8cc707d318f92f9672d450f9d0fc1
Graeme Stirling <graemejstirling@gmail.com>
2023-01-11 12:10:40 +0000

    Update files to 2023 (#359)

--- app/uk/gov/hmrc/fhregistrationfrontend/forms/journey/Page.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/forms/journey/Page.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022 HM Revenue & Customs
+ * Copyright 2023 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -110,9 +110,8 @@ object Page {
         override def render(form: Form[ContactPerson], bpr: BusinessRegistrationDetails, navigation: Navigation)(
           implicit request: Request[_],
           messages: Messages,
-          appConfig: AppConfig): Html = {
+          appConfig: AppConfig): Html =
           views.contact_person(form, bpr, navigation)
-        }
       },
       addressOnPage = _.otherUkContactAddress
     )
