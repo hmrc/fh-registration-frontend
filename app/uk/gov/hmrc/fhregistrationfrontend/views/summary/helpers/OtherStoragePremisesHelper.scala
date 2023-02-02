a8960de62789fbd4bbab09313ff43a63bba5fd86
Graeme Stirling <graemejstirling@gmail.com>
2023-02-02 16:41:01 +0000

    update links/OtherStoragePremisesPage

---
+++ app/uk/gov/hmrc/fhregistrationfrontend/views/summary/helpers/OtherStoragePremisesHelper.scala
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.views.summary.helpers
+
+import uk.gov.hmrc.fhregistrationfrontend.forms.models.{OtherStoragePremises => StoragePremisesModel}
+import uk.gov.hmrc.fhregistrationfrontend.views.helpers._
+import uk.gov.hmrc.fhregistrationfrontend.forms.models.StoragePremise
+import uk.gov.hmrc.fhregistrationfrontend.views.Mode
+import uk.gov.hmrc.fhregistrationfrontend.views.Mode.{Mode, New}
+import uk.gov.hmrc.fhregistrationfrontend.views.summary.GroupRow
+import uk.gov.hmrc.govukfrontend.views.html.components.{ActionItem, Actions, Text}
+import play.api.i18n.Messages
+
+object OtherStoragePremisesHelper {
+
+  def apply(data: StoragePremisesModel, mode: Mode)(implicit messages: Messages) = {
+
+    val changeLink = {
+      if (Mode isEditable mode) {
+        Some("form/otherStoragePremises")
+      } else {
+        None
+      }
+    }
+
+    val storageAddress = {
+      data.value.values.zipWithIndex.flatMap {
+        case (storagePremise: StoragePremise, index) =>
+          val address = Helpers.findAddress(Some(storagePremise.address))
+
+          Seq(
+            Helpers.createSummaryRow(
+              SummaryRowParams.ofBoolean(
+                Some(Messages("fh.summary.thirdPartyPremises")),
+                storagePremise.isThirdParty,
+                changeLink,
+                GroupRow.Bottom
+              ),
+              None
+            ),
+            Helpers.createSummaryRow(
+              SummaryRowParams(Some(Messages("fh.other_storage_premises.each.title", {
+                index + 1
+              })), Some(address), None, GroupRow.Single),
+              Some(
+                Actions(
+                  items = Seq(
+                    ActionItem(
+                      href = changeLink.get + s"/${index + 1}",
+                      content = Text("Change"),
+                      visuallyHiddenText = Some(Messages("fh.other_storage_premises.each.title", {
+                        index + 1
+                      }))
+                    )
+                  )
+                ))
+            )
+          )
+      }.toSeq
+    }
+
+    val storagePremise = Seq(
+      Helpers.createSummaryRow(
+        SummaryRowParams.ofBoolean(
+          Some(Messages("fh.summary.usesStoragePremises")),
+          data.hasValue,
+          changeLink,
+          GroupRow.Single
+        ),
+        None))
+
+    if (data.hasValue) {
+      storageAddress ++ storagePremise
+    } else {
+      storagePremise
+    }
+  }
+}
