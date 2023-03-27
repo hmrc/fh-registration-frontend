c9dc2a833c687d5025bd0602d552361d6b3a0838
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-03-27 14:26:41 +0100

    Tidied up commented out code

--- it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerAddressControllerIntegrationSpec.scala
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerAddressControllerIntegrationSpec.scala
@@ -113,41 +113,4 @@ class BusinessPartnerAddressControllerIntegrationSpec
       }
     }
   }
-
-
 }
-    /*"Show the summary page when the user has fulfilled all the pages" in {
-
-      given
-        .summaryPrecondition
-
-      WsTestClient.withClient { client =>
-        val result = client.url(s"$baseUrl/summary")
-          .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-          .get()
-
-        whenReady(result) { res =>
-          res.status mustBe 200
-        }
-
-      }
-    }
-
-    "Show the bad request page when the user has not fulfilled all the pages" in {
-      given
-        .commonPrecondition
-
-      WsTestClient.withClient { client =>
-        val result = client.url(s"$baseUrl/summary")
-          .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
-          .get()
-
-        whenReady(result) { res =>
-          res.status mustBe 400
-        }
-
-      }
-    }
-  }
-
-}*/
