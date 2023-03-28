59ad22ef9926880f175a2a83c710f9295e35c287
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2023-03-28 13:56:42 +0100

    DLS-7107 added partners vat number page

--- app/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersVatRegistrationNumberController.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersVatRegistrationNumberController.scala
@@ -19,15 +19,14 @@ package uk.gov.hmrc.fhregistrationfrontend.controllers
 import com.google.inject.{Inject, Singleton}
 import play.api.data.FormError
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Results}
-import play.filters.csrf.CSRF.ErrorHandler
 import uk.gov.hmrc.fhregistrationfrontend.actions.Actions
 import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
-import uk.gov.hmrc.fhregistrationfrontend.forms.definitions.BusinessPartnersChooseAddressForm.{chooseAddressForm, chooseAddressKey}
-import uk.gov.hmrc.fhregistrationfrontend.forms.models.Address
+import uk.gov.hmrc.fhregistrationfrontend.forms.definitions.VatNumberForm.{vatNumberForm, vatNumberKey}
+import uk.gov.hmrc.fhregistrationfrontend.forms.models.VatNumber
 import uk.gov.hmrc.fhregistrationfrontend.views.Views
 
 @Singleton
-class BusinessPartnersChooseAddressController @Inject()(
+class BusinessPartnersVatRegistrationNumberController @Inject()(
   ds: CommonPlayDependencies,
   view: Views,
   actions: Actions,
@@ -38,10 +37,10 @@ class BusinessPartnersChooseAddressController @Inject()(
 
   def load(): Action[AnyContent] = userAction { implicit request =>
     if (config.newBusinessParnerPagesEnabled) {
-      val form = chooseAddressForm
+      val form = vatNumberForm
       //ToDo read this data from the cache after being stored before the redirect
-      val addressList = testAddressData
-      Ok(view.business_partner_choose_address(form, addressList))
+      val partnerName = "test partner"
+      Ok(view.business_partner_vat_registration(form, partnerName))
     } else {
       errorHandler.errorResultsPages(Results.NotFound)
     }
@@ -50,19 +49,16 @@ class BusinessPartnersChooseAddressController @Inject()(
   def next(): Action[AnyContent] = userAction { implicit request =>
     if (config.newBusinessParnerPagesEnabled) {
       //ToDo read this data from the cache after being stored before the redirect
-      val addressList = testAddressData
-
-      chooseAddressForm.bindFromRequest.fold(
+      val partnerName = "test partner"
+      vatNumberForm.bindFromRequest.fold(
         formWithErrors => {
-          BadRequest(view.business_partner_choose_address(formWithErrors, testAddressData))
+          BadRequest(view.business_partner_vat_registration(formWithErrors, partnerName))
         },
-        addressKey => {
-          addressList.get(addressKey.chosenAddress) match {
-            case Some(address) => Ok(s"Next page! with form result: ${address.toString}")
+        vatNumber => {
+          vatNumber.value match {
+            case Some(vatNumber) => Ok(s"Next page! with vatNumber: $vatNumber")
             case None =>
-              val formWithError =
-                chooseAddressForm.withError(FormError("chosenAddress", "error.required"))
-              BadRequest(view.business_partner_choose_address(formWithError, testAddressData))
+              Ok(s"Next page! with no vatNumber")
           }
         }
       )
@@ -70,31 +66,4 @@ class BusinessPartnersChooseAddressController @Inject()(
       errorHandler.errorResultsPages(Results.NotFound)
     }
   }
-
-  //ToDo remove when addressData stored in database
-  private val testAddressData: Map[String, Address] = {
-    val address1 = Address(
-      addressLine1 = "1 Romford Road",
-      addressLine2 = Some("Wellington"),
-      addressLine3 = Some("Telford"),
-      addressLine4 = None,
-      postcode = "TF1 4ER",
-      countryCode = None,
-      lookupId = None
-    )
-
-    val address2 = address1.copy(addressLine1 = "2 Romford Road")
-    val address3 = address1.copy(addressLine1 = "3 Romford Road")
-    val address4 = address1.copy(addressLine1 = "2 Romford Road")
-    val address5 = address1.copy(addressLine1 = "5 Romford Road")
-
-    Map(
-      "1" -> address1,
-      "2" -> address2,
-      "3" -> address3,
-      "4" -> address4,
-      "5" -> address5
-    )
-  }
-
 }
