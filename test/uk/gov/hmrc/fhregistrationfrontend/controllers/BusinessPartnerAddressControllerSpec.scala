2a75f8eee85618d1628a3115ad09230d03fc44d9
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-03-28 11:47:01 +0100

    Fix compilation issues after resolving merge conflicts

--- test/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerAddressControllerSpec.scala
+++ test/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerAddressControllerSpec.scala
@@ -39,7 +39,7 @@ class BusinessPartnerAddressControllerSpec extends ControllerSpecWithGuiceApp wi
     "Render the business partner address page" when {
       "the new business partner pages are enabled" in {
         setupUserAction()
-        when(mockAppConfig.newBusinessParnerPagesEnabled).thenReturn(true)
+        when(mockAppConfig.newBusinessPartnerPagesEnabled).thenReturn(true)
         val request = FakeRequest()
         val result = await(csrfAddToken(controller.load())(request))
 
@@ -53,7 +53,7 @@ class BusinessPartnerAddressControllerSpec extends ControllerSpecWithGuiceApp wi
     "Render the not found page" when {
       "the new business partner pages are disabled" in {
         setupUserAction()
-        when(mockAppConfig.newBusinessParnerPagesEnabled).thenReturn(false)
+        when(mockAppConfig.newBusinessPartnerPagesEnabled).thenReturn(false)
         val request = FakeRequest()
         val result = await(csrfAddToken(controller.load())(request))
 
@@ -69,7 +69,7 @@ class BusinessPartnerAddressControllerSpec extends ControllerSpecWithGuiceApp wi
     "the new business partner pages are enabled" should {
       "return 200" when {
         setupUserAction()
-        when(mockAppConfig.newBusinessParnerPagesEnabled).thenReturn(true)
+        when(mockAppConfig.newBusinessPartnerPagesEnabled).thenReturn(true)
         val request = FakeRequest()
           .withFormUrlEncodedBody(("partnerPostcode", "AB1 2YZ"))
         val result = await(csrfAddToken(controller.load())(request))
@@ -84,7 +84,7 @@ class BusinessPartnerAddressControllerSpec extends ControllerSpecWithGuiceApp wi
     "Render the not found page" when {
       "the new business partner pages are disabled" in {
         setupUserAction()
-        when(mockAppConfig.newBusinessParnerPagesEnabled).thenReturn(false)
+        when(mockAppConfig.newBusinessPartnerPagesEnabled).thenReturn(false)
         val request = FakeRequest()
         val result = await(csrfAddToken(controller.load())(request))
 
