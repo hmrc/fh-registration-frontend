d67f15fe888067f59165a41542f42704daba8b5a
Graeme Stirling <graemejstirling@gmail.com>
2023-03-28 11:24:22 +0100

    Add route, controller and test for conformation page

--- app/uk/gov/hmrc/fhregistrationfrontend/views/Views.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/views/Views.scala
@@ -78,4 +78,5 @@ class Views @Inject()(
   val show_submission: uk.gov.hmrc.fhregistrationfrontend.views.html.show_submission,
 ////////////////////////////BusinessParners///////////////////////////////////////////////
   val business_partner_choose_address: uk.gov.hmrc.fhregistrationfrontend.views.html.businessPartners.v2.business_partners_choose_address
+  // val business_partner_confirm_partner_address: uk.gov.hmrc.fhregistrationfrontend.views.html.businessPartners.v2.business_partners_choose_address
 )
---
+++ test/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersConfirmAddressControllerSpec.scala
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import com.codahale.metrics.SharedMetricRegistries
+import org.jsoup.Jsoup
+import org.mockito.Mockito.{reset, when}
+import play.api.test.FakeRequest
+import play.api.test.Helpers.{contentAsString, defaultAwaitTimeout}
+import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
+import uk.gov.hmrc.fhregistrationfrontend.teststubs.ActionsMock
+import uk.gov.hmrc.fhregistrationfrontend.views.Views
+
+class BusinessPartnersConfirmAddressControllerSpec extends ControllerSpecWithGuiceApp with ActionsMock {
+
+  SharedMetricRegistries.clear()
+
+  override lazy val views = app.injector.instanceOf[Views]
+
+  val mockAppConfig = mock[FrontendAppConfig]
+
+  val controller =
+    new BusinessPartnersConfirmAddressController(commonDependencies, views, mockActions, mockAppConfig)(mockMcc)
+
+  "load" should {
+    "Render the confirm address page" when {
+      "the new business partner pages are enabled" in {
+        // Make fake request
+        setupUserAction()
+        // when(mockAppConfig.newBusinessParnerPagesEnabled).thenReturn(true)
+        val request = FakeRequest()
+        val result = await(csrfAddToken(controller.load())(request))
+
+        // if successful status should be 200
+        // page title should also appear
+        status(result) shouldBe OK
+      }
+    }
+  }
+}
