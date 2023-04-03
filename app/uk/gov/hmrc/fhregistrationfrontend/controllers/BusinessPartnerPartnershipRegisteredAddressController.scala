5cdcbf1e49ecd6d128d1bd89e5db7a37ef5e58ae
Graeme Stirling <graemejstirling@gmail.com>
2023-04-03 15:19:11 +0100

    rebase

---
+++ app/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerPartnershipRegisteredAddressController.scala
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import play.api.mvc._
+import uk.gov.hmrc.fhregistrationfrontend.actions.Actions
+import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
+import uk.gov.hmrc.fhregistrationfrontend.forms.definitions.BusinessPartnersAddressForm.businessPartnersAddressForm
+import uk.gov.hmrc.fhregistrationfrontend.views.Views
+
+import javax.inject.Inject
+
+class BusinessPartnerPartnershipRegisteredAddressController @Inject()(
+  ds: CommonPlayDependencies,
+  view: Views,
+  actions: Actions,
+  config: FrontendAppConfig)(
+  cc: MessagesControllerComponents
+) extends AppController(ds, cc) {
+
+  import actions._
+  def load(): Action[AnyContent] = userAction { implicit request =>
+    if (config.newBusinessPartnerPagesEnabled) {
+      // Todo get this from cache later
+      val partnerName = "Test User"
+      val bpAddressForm = businessPartnersAddressForm
+      val postAction =
+        Call(
+          method = "POST",
+          url = uk.gov.hmrc.fhregistrationfrontend.controllers.routes.BusinessPartnerAddressController.load().url
+        )
+      Ok(view.business_partner_partnership_registered_address(bpAddressForm, postAction, partnerName))
+    } else {
+      errorHandler.errorResultsPages(Results.NotFound)
+    }
+  }
+
+}
