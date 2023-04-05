0272edaf2196537a27af0e2d53bf31b56915fd6c
Graeme Stirling <graemejstirling@gmail.com>
2023-04-05 16:15:09 +0100

    add it load page it test

---
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerPartnershipRegisteredAddressControllerISpec.scala
@@ -0,0 +1,38 @@
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import org.jsoup.Jsoup
+import play.api.libs.ws.DefaultWSCookie
+import play.api.test.WsTestClient
+import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
+
+class BusinessPartnerPartnershipRegisteredAddressControllerISpec
+  extends Specifications with TestConfiguration {
+
+  val requestURL = "/form/business-partners/partnership-registered-office-address"
+
+  "GET /form/business-partners/partnership-registered-office-address" when {
+
+    "the new business partners flow is enabled" should {
+
+      "render the business partner address page" when {
+        "the user is authenticated" in {
+          given
+            .commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl$requestURL")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              val page = Jsoup.parse(res.body)
+              page.title() must include("What is the partner’s address?")
+              page.getElementById("page-heading").text() must include("What is Test User’s registered office address?")
+            }
+          }
+        }
+      }
+    }
+  }
+}
