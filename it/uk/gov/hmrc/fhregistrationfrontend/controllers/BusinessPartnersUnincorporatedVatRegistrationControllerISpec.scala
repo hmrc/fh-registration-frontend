938dfc573942462cf4d34735b1b53b87ebaff52d
Sean Murray <sean.murray@digital.hmrc.gov.uk>
2023-05-09 11:28:36 +0100

    [SM][DLS-7131] changed the name of the it:test spec to fit in with the others.
    Signed-off-by: Sean Murray <sean.murray@digital.hmrc.gov.uk>

--- it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersUnincorporatedVatRegistrationControllerISpec.scala
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersUnincorporatedVatRegistrationControllerISpec.scala
@@ -5,7 +5,7 @@ import play.api.libs.ws.DefaultWSCookie
 import play.api.test.WsTestClient
 import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
 
-class BusinessPartnersUnincorporatedVatRegistrationControllerSpec
+class BusinessPartnersUnincorporatedVatRegistrationControllerISpec
   extends Specifications with TestConfiguration {
 
   "GET /form/business-partners/unincorporated-body-vat-registration-number" should {
