bb0d4e45d7bebef3d6afac42d504f2bcb5f65b38
Graeme Stirling <graemejstirling@gmail.com>
2023-05-09 16:31:25 +0100

    create controller, page and GET route

---
+++ test/uk/gov/hmrc/fhregistrationfrontend/controllers/TradingNameControllerSpec.scala
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import com.codahale.metrics.SharedMetricRegistries
+import org.jsoup.Jsoup
+import org.mockito.Mockito.{reset, when}
+import org.scalatest.BeforeAndAfterEach
+import play.api.test.FakeRequest
+import uk.gov.hmrc.fhregistrationfrontend.forms.definitions.EmailVerificationForm
+import uk.gov.hmrc.fhregistrationfrontend.services.Save4LaterService
+import uk.gov.hmrc.fhregistrationfrontend.teststubs.{ActionsMock, EmailVerificationConnectorMocks, InMemoryShortLivedCache}
+import uk.gov.hmrc.http.HeaderCarrier
+import play.api.test.Helpers._
+import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
+import uk.gov.hmrc.fhregistrationfrontend.views.Views
+
+class TradingNameControllerSpec
+    extends ControllerSpecWithGuiceApp with EmailVerificationConnectorMocks with ActionsMock with BeforeAndAfterEach {
+
+  SharedMetricRegistries.clear()
+
+  override lazy val views = app.injector.instanceOf[Views]
+  lazy val mockAppConfig = mock[FrontendAppConfig]
+
+  val controller = new TradingNameController(commonDependencies, views, mockActions, mockAppConfig)(mockMcc)
+
+  "load" should {
+    "Render the business partner trading name page" when {
+      "The business partner v2 pages are enabled" in {
+        setupUserAction()
+
+        when(mockAppConfig.newBusinessPartnerPagesEnabled).thenReturn(true)
+        val request = FakeRequest()
+        val result = await(csrfAddToken(controller.load())(request))
+
+        status(result) shouldBe OK
+        val page = Jsoup.parse(contentAsString(result))
+        page.text() should include("Page has loaded")
+        reset(mockActions)
+      }
+    }
+  }
+}
