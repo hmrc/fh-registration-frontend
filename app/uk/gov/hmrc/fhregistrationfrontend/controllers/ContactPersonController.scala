69311230ffd04f610c9097e9c127a2cd64842e6c
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-05-11 15:16:28 +0100

    Initial commit

---
+++ app/uk/gov/hmrc/fhregistrationfrontend/controllers/ContactPersonController.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import play.api.mvc._
+import uk.gov.hmrc.fhregistrationfrontend.actions.Actions
+import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
+import uk.gov.hmrc.fhregistrationfrontend.forms.definitions.ContactPersonForm.contactPersonForm
+import uk.gov.hmrc.fhregistrationfrontend.views.Views
+import uk.gov.hmrc.fhregistrationfrontend.models.businessregistration.{Address, BusinessRegistrationDetails}
+import uk.gov.hmrc.fhregistrationfrontend.forms.models.Address
+
+import javax.inject.Inject
+
+class TradingNameController @Inject()(
+  ds: CommonPlayDependencies,
+  view: Views,
+  actions: Actions,
+  config: FrontendAppConfig)(
+  cc: MessagesControllerComponents
+) extends AppController(ds, cc) {
+
+  import actions._
+
+  val address: Address = Address(
+    addressLine1 = "23 High Street",
+    addressLine2 = Some("Park View"),
+    addressLine3 = Some("Gloucester"),
+    addressLine4 = Some("Gloucestershire"),
+    postcode = "NE98 1ZZ",
+    countryCode = None,
+    lookupId = None
+  )
+
+  def load(): Action[AnyContent] = userAction { implicit request =>
+    if (config.newBusinessPartnerPagesEnabled) {
+      Ok(view.contact_person(contactPersonForm, address))
+    } else {
+      errorHandler.errorResultsPages(Results.NotFound)
+    }
+  }
+  def next(): Action[AnyContent] = userAction { implicit request =>
+    if (config.newBusinessPartnerPagesEnabled) {
+      val bpr = BusinessRegistrationDetails.apply()
+      contactPersonForm.bindFromRequest.fold(
+        formWithErrors => {
+          BadRequest(view.contact_person(formWithErrors, address))
+        },
+        contactPerson => {
+          Ok(s"Form submitted, with result: $contactPerson")
+        }
+      )
+    } else {
+      errorHandler.errorResultsPages(Results.NotFound)
+    }
+  }
+
+}
--- app/uk/gov/hmrc/fhregistrationfrontend/views/Views.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/views/Views.scala
@@ -74,6 +74,7 @@ class Views @Inject()(
   val temp_admin_page: uk.gov.hmrc.fhregistrationfrontend.views.html.temp_admin_page,
   val show_all_submissions: uk.gov.hmrc.fhregistrationfrontend.views.html.show_all_submissions,
   val show_submission: uk.gov.hmrc.fhregistrationfrontend.views.html.show_submission,
+  val contact_person_page: uk.gov.hmrc.fhregistrationfrontend.views.html.contact_person,
 ////////////////////////////BusinessPartners///////////////////////////////////////////////
   val business_partner_choose_address: uk.gov.hmrc.fhregistrationfrontend.views.html.businessPartners.v2.business_partners_choose_address,
   val business_partner_vat_registration: uk.gov.hmrc.fhregistrationfrontend.views.html.businessPartners.v2.business_partners_vat_registration,
