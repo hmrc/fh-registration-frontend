f8686d2c805109ae171a800a85ea0a901accd973
SeanMurray1 <sean.murray@digital.hmrc.gov.uk>
2023-05-15 15:31:34 +0100

    [SM][DLS-7883] added in new route a new route and controller for vatN… (#403)
    * [SM][DLS-7883] added in new route a new route and controller for vatNumber.
    
    Signed-off-by: Sean Murray <sean.murray@digital.hmrc.gov.uk>
    
    * [SM][DLS-7883] removed the corporateBody val as it is not required. Also added in a temporary match for the backUrl until navigation is added to the UkVatNumberController.
    
    Signed-off-by: Sean Murray <sean.murray@digital.hmrc.gov.uk>
    
    ---------
    
    Signed-off-by: Sean Murray <sean.murray@digital.hmrc.gov.uk>

---
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/UkVatNumberControllerISpec.scala
@@ -0,0 +1,159 @@
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import org.jsoup.Jsoup
+import play.api.libs.ws.DefaultWSCookie
+import play.api.test.WsTestClient
+import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
+
+class UkVatNumberControllerISpec
+  extends Specifications with TestConfiguration {
+
+  "GET /form/business-partners/corporate-body-vat-registration-number" should {
+
+    "render the corporate-body-vat-registration-number page" in {
+      given
+        .commonPrecondition
+
+      WsTestClient.withClient { client =>
+        val result = client.url(s"$baseUrl/vatNumber ")
+          .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+          .get()
+
+        whenReady(result) { res =>
+          res.status mustBe 200
+          val page = Jsoup.parse(res.body)
+          page.title() must include("Does the business have a UK VAT registration number?")
+          page.getElementsByTag("h1").text() must include("Does the business have a UK VAT registration number?")
+        }
+      }
+    }
+  }
+
+  "POST /form/business-partners/corporate-body-vat-registration-number" when {
+    "yes is selected and the vatnumber entered" should {
+      "return 200 with vatnumber" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/vatNumber ")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+            .withHttpHeaders(xSessionId,
+              "Csrf-Token" -> "nocheck")
+            .post(Map("vatNumber_yesNo" -> Seq("true"),
+              "vatNumber_value" -> Seq("123456789")))
+
+          whenReady(result) { res =>
+            res.status mustBe 200
+            res.body mustBe "Next page!"
+          }
+        }
+      }
+    }
+
+    "no is selected" should {
+      "return 200 with no vatnumber message" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/vatNumber")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+            .withHttpHeaders(xSessionId,
+              "Csrf-Token" -> "nocheck")
+            .post(Map("vatNumber_yesNo" -> Seq("false")))
+
+          whenReady(result) { res =>
+            res.status mustBe 200
+            res.body mustBe "Next page!"
+          }
+        }
+      }
+    }
+
+    "no hasVat number selected" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/vatNumber")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+            .withHttpHeaders(xSessionId,
+              "Csrf-Token" -> "nocheck")
+            .post(Map("vatNumber_yesNo" -> Seq.empty))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("There is a problem Select whether the business has a VAT registration number")
+          }
+        }
+      }
+    }
+
+    "hasVat number selected but vatNumber not supplied" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/vatNumber")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+            .withHttpHeaders(xSessionId,
+              "Csrf-Token" -> "nocheck")
+            .post(Map("vatNumber_yesNo" -> Seq("true")))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("There is a problem Enter the VAT registration number")
+          }
+        }
+      }
+    }
+
+    "the form hasVat field is invalid format" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/vatNumber")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+            .withHttpHeaders(xSessionId,
+              "Csrf-Token" -> "nocheck")
+            .post(Map("vatNumber_yesNo" -> Seq("error")))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("There is a problem Select whether the business has a VAT registration number")
+          }
+        }
+      }
+    }
+
+    "the vatnumber field is invalid format" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/vatNumber")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+            .withHttpHeaders(xSessionId,
+              "Csrf-Token" -> "nocheck")
+            .post(Map("vatNumber_yesNo" -> Seq("true"),
+              "vatNumber_value" -> Seq("1234")))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("There is a problem Enter a valid UK VAT registration number")
+          }
+        }
+      }
+    }
+  }
+}
