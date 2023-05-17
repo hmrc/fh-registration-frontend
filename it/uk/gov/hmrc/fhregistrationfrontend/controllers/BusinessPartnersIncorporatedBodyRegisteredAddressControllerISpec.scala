13252e950a04dfc51d5295b4c03884f94ce70ab5
Graeme Stirling <graemejstirling@gmail.com>
2023-05-17 09:43:16 +0100

    Graeme/dls 7133 (#404)
    * add controller and tests

---
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersIncorporatedBodyRegisteredAddressControllerISpec.scala
@@ -0,0 +1,164 @@
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import org.jsoup.Jsoup
+import play.api.libs.ws.DefaultWSCookie
+import play.api.test.WsTestClient
+import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
+
+class BusinessPartnersIncorporatedBodyRegisteredAddressControllerISpec
+  extends Specifications with TestConfiguration {
+
+  val requestURL = "/form/business-partners/unincorporated-body-registered-office-address"
+
+  "GET /form/business-partners/unincorporated-body-registered-office-address" when {
+
+    "the new business partners flow is enabled" should {
+
+      "render the business partner address page" when {
+        "the user is authenticated" in {
+          given
+            .commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl$requestURL")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              val page = Jsoup.parse(res.body)
+              page.title() must include("What is the unincorporated body’s registered office address?")
+              page.getElementsByTag("h1").text() must include("What is Test incorporated Body’s registered office address?")
+            }
+          }
+        }
+      }
+    }
+  }
+
+  "POST /form/business-partners/unincorporated-body-registered-office-address" when {
+    "the form has no errors" should {
+      "return 200" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl$requestURL")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).withHttpHeaders(xSessionId,
+            "Csrf-Token" -> "nocheck")
+            .post(Map("partnerAddressLine" -> Seq("1"),
+              "partnerPostcode" -> Seq("AB1 2YZ")))
+
+          whenReady(result) { res =>
+            res.status mustBe 200
+            res.body mustBe "Next page! with postcode: AB1 2YZ and address line 1"
+          }
+        }
+      }
+    }
+
+    "address line not populated" should {
+      "return 200" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl$requestURL")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).withHttpHeaders(xSessionId,
+            "Csrf-Token" -> "nocheck")
+            .post(Map("partnerAddressLine" -> Seq.empty,
+              "partnerPostcode" -> Seq("AB1 2YZ")))
+
+          whenReady(result) { res =>
+            res.status mustBe 200
+            res.body mustBe "Next page! with postcode: AB1 2YZ and no address line"
+          }
+        }
+      }
+    }
+
+    "postcode not populated" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl$requestURL")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).withHttpHeaders(xSessionId,
+            "Csrf-Token" -> "nocheck")
+            .post(Map("partnerAddressLine" -> Seq("1"),
+              "partnerPostcode" -> Seq.empty))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("Enter the postcode of the address")
+          }
+        }
+      }
+    }
+
+    "postcode invalid format" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl$requestURL")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).withHttpHeaders(xSessionId,
+            "Csrf-Token" -> "nocheck")
+            .post(Map("partnerAddressLine" -> Seq("1"),
+              "partnerPostcode" -> Seq("A")))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("There is a problem Enter a valid postcode")
+          }
+        }
+      }
+    }
+
+    "address line contains invalid characters" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl$requestURL")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).withHttpHeaders(xSessionId,
+            "Csrf-Token" -> "nocheck")
+            .post(Map("partnerAddressLine" -> Seq("The lane;"),
+              "partnerPostcode" -> Seq("AB1 2YZ")))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("only include letters a to z, numbers, apostrophes, commas, dashes, exclamation marks, forward slashes, full stops, hyphens, quotation marks, round brackets and spaces")
+          }
+        }
+      }
+    }
+
+    "address line contains more than 35 characters" should {
+      "return 400" in {
+        given
+          .commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl$requestURL")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).withHttpHeaders(xSessionId,
+            "Csrf-Token" -> "nocheck")
+            .post(Map("partnerAddressLine" -> Seq("qwertyuiopasdfghjklzxcvbnmqwkydvkdsgvisudgfkjsdvkjsdcjkdh"),
+              "partnerPostcode" -> Seq("AB1 2YZ")))
+
+          whenReady(result) { res =>
+            res.status mustBe 400
+            val page = Jsoup.parse(res.body)
+            page.getElementsByClass("govuk-error-summary").text() must include("Address lines must not be longer than 35 characters")
+          }
+        }
+      }
+    }
+  }
+}
