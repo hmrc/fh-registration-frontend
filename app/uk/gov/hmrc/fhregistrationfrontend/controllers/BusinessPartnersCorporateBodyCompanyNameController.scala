50d9b74cbd668e734b7e614ed85a49e0fe17f669
Graeme Stirling <graemejstirling@gmail.com>
2023-05-25 11:47:08 +0100

    controller, view and tests

---
+++ app/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersCorporateBodyCompanyNameController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import com.google.inject.{Inject, Singleton}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents, Results}
+import uk.gov.hmrc.fhregistrationfrontend.actions.Actions
+import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
+import uk.gov.hmrc.fhregistrationfrontend.views.Views
+import uk.gov.hmrc.fhregistrationfrontend.forms.definitions.CompanyNameForm.{companyNameForm, companyNameKey}
+
+@Singleton
+class BusinessPartnersCorporateBodyCompanyNameController @Inject()(
+  ds: CommonPlayDependencies,
+  view: Views,
+  actions: Actions,
+  config: FrontendAppConfig)(
+  cc: MessagesControllerComponents
+) extends AppController(ds, cc) {
+  import actions._
+
+  val journeyType = "corporateBody"
+  val postAction = routes.BusinessPartnersCorporateBodyCompanyNameController.next()
+
+  def load(): Action[AnyContent] = userAction { implicit request =>
+    if (config.newBusinessPartnerPagesEnabled) {
+      Ok(view.business_partners_company_name(journeyType, postAction, companyNameForm, companyNameKey))
+    } else {
+      errorHandler.errorResultsPages(Results.NotFound)
+    }
+  }
+
+  def next(): Action[AnyContent] = userAction { implicit request =>
+    if (config.newBusinessPartnerPagesEnabled) {
+      companyNameForm.bindFromRequest.fold(
+        formWithErrors => {
+          BadRequest(view.business_partners_company_name(journeyType, postAction, formWithErrors, companyNameKey))
+        },
+        companyName => {
+          Ok(s"Form submitted, with result: $companyName")
+        }
+      )
+    } else {
+      errorHandler.errorResultsPages(Results.NotFound)
+    }
+  }
+}
