fd207abab90a75fe1f0b6e8089bbe580b3aa2a59
Sean Murray <sean.murray@digital.hmrc.gov.uk>
2023-07-12 15:15:01 +0100

    [SM][DLS-8228] WIP business partner page

---
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerControllerIntegrationSpec.scala
@@ -0,0 +1,57 @@
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import org.jsoup.Jsoup
+import play.api.libs.ws.DefaultWSCookie
+import play.api.test.WsTestClient
+import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
+
+class BusinessPartnerControllerIntegrationSpec
+  extends Specifications with TestConfiguration {
+
+
+  "GET /form/business-partner" when {
+
+    "render the business partners page" when {
+      "the user is authenticated" in {
+        given.commonPrecondition
+
+        WsTestClient.withClient { client =>
+          val result = client.url(s"$baseUrl/business-partners")
+            .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie)).get()
+
+          whenReady(result) { res =>
+            res.status mustBe 200
+            val page = Jsoup.parse(res.body)
+            page.title must include("Business partners - Apply for the Fulfilment House Due Diligence Scheme")
+          }
+        }
+      }
+    }
+
+  }
+
+  "POST /business-partners" when {
+
+    "the user clicks save and continue" should {
+      "return 200" when {
+        "the user is authenticated" in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "mock" -> Seq("true"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+      }
+    }
+  }
+}
