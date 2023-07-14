c108878c3f83c25d28b74eb7f035730ed5ab0d30
Sean Murray <sean.murray@digital.hmrc.gov.uk>
2023-07-14 16:49:59 +0100

    [SM][DLS-8228] Added more tests for each option as well as for the error scenario.

--- it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerControllerIntegrationSpec.scala
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnerControllerIntegrationSpec.scala
@@ -34,7 +34,7 @@ class BusinessPartnerControllerIntegrationSpec
 
     "the user clicks save and continue" should {
       "return 200" when {
-        "the user is authenticated" in {
+        "the user is authenticated and select Individual" in {
           given.commonPrecondition
 
           WsTestClient.withClient { client =>
@@ -48,6 +48,112 @@ class BusinessPartnerControllerIntegrationSpec
             whenReady(result) { res =>
               res.status mustBe 200
               res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+
+        "the user is authenticated and select SoleProprietor " in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "businessType" -> Seq("SoleProprietor"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+        "the user is authenticated and select Partnership " in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "businessType" -> Seq("Partnership"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+        "the user is authenticated and select Limited Liability Partnership " in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "businessType" -> Seq("LimitedLiabilityPartnership"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+        "the user is authenticated and select Corporate Body " in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "businessType" -> Seq("CorporateBody"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+        "the user is authenticated and select Unincorporated Body " in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "businessType" -> Seq("UnincorporatedBody"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+      }
+
+      "return 400" when {
+        "the user is authenticated and Nothing is selected" in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "" -> Seq(""),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 400
+              res.body must include("Select the type of legal entity for the partner")
             }
           }
         }
