958450aaf6abe86a07fa8baf4eab201031f9fcb6
jackwillett1 <jack.willett1@digital.hmrc.gov.uk>
2023-09-11 15:44:30 +0100

    Added tests for new page

---
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersCheckYourAnswersControllerISpec.scala
@@ -0,0 +1,60 @@
+package uk.gov.hmrc.fhregistrationfrontend.controllers
+
+import org.jsoup.Jsoup
+import play.api.libs.ws.DefaultWSCookie
+import play.api.test.WsTestClient
+import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
+
+class BusinessPartnersCheckYourAnswersControllerISpec
+  extends Specifications with TestConfiguration {
+
+  "GET /business-partners/check-your-answers" when {
+
+    "the new business partners flow is enabled" should {
+
+      "render the Check Your Answers page" when {
+        "the user is authenticated" in {
+          given
+            .commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners/check-your-answers")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .get()
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              val page = Jsoup.parse(res.body)
+              page.title() must include("Check your answers")
+            }
+          }
+        }
+      }
+    }
+  }
+
+  "POST /business-partners/check-your-answers" when {
+
+    "the user clicks save and continue" should {
+      "return 200" when {
+        "the user is authenticated" in {
+          given.commonPrecondition
+
+          WsTestClient.withClient { client =>
+            val result = client.url(s"$baseUrl/business-partners/check-your-answers")
+              .addCookies(DefaultWSCookie("mdtp", authAndSessionCookie))
+              .withHttpHeaders(xSessionId, "Csrf-Token" -> "nocheck")
+              .post(Map(
+                "mock" -> Seq("true"),
+              ))
+
+            whenReady(result) { res =>
+              res.status mustBe 200
+              res.body must include("Form submitted, with result:")
+            }
+          }
+        }
+      }
+    }
+  }
+}
