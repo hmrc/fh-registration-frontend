dee191a000a15ae7fb33bd023cb02956ef91a558
Graeme Stirling <graemejstirling@gmail.com>
2023-10-12 16:25:25 +0100

    review comments

--- it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersCheckYourAnswersControllerISpec.scala
+++ it/uk/gov/hmrc/fhregistrationfrontend/controllers/BusinessPartnersCheckYourAnswersControllerISpec.scala
@@ -1,20 +1,25 @@
 package uk.gov.hmrc.fhregistrationfrontend.controllers
 
 import org.jsoup.Jsoup
+import org.mockito.Mockito.when
+import org.scalatestplus.mockito.MockitoSugar.mock
 import play.api.libs.ws.DefaultWSCookie
 import play.api.test.WsTestClient
+import uk.gov.hmrc.fhregistrationfrontend.config.FrontendAppConfig
 import uk.gov.hmrc.fhregistrationfrontend.testsupport.{Specifications, TestConfiguration}
 
 class BusinessPartnersCheckYourAnswersControllerISpec
   extends Specifications with TestConfiguration {
 
   val route = "/business-partners/check-your-answers"
+  lazy val mockAppConfig = mock[FrontendAppConfig]
 
   s"GET $route" when {
     "the new business partners flow is enabled" should {
       "render the Check Your Answers page" when {
         "the user is authenticated" in {
           given.commonPrecondition
+          when(mockAppConfig.getRandomBusinessType()).thenReturn("individual")
 
           WsTestClient.withClient { client =>
             val result = client.url(baseUrl + route)
@@ -25,6 +30,11 @@ class BusinessPartnersCheckYourAnswersControllerISpec
               res.status mustBe 200
               val page = Jsoup.parse(res.body)
               page.title() must include("Check your answers")
+              page.body().text() must include("Type of partner Individual")
+              page.body().text() must include("First name first name")
+              page.body().text() must include("Last name last name")
+              page.body().text() must include("National Insurance number QQ123456C")
+              page.body().text() must include(" Address 1 Romford Road Wellington Telford TF1 4ER")
             }
           }
         }
