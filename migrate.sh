20ab222ca007b77b149b197c0678613b2f767555
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2024-01-31 15:59:54 +0000

    DLS-9792 added tests

--- migrate.sh
+++ migrate.sh
@@ -16,14 +16,14 @@ echo ""
 echo "Moving test files from generated-test/ to test/"
 echo ""
 
-rsync -avm --include='*.scala' -f 'hide,! */' ../generated-test/ ../test/uk.gov.hmrc.fhregistrationfrontend
+rsync -avm --include='*.scala' -f 'hide,! */' ../generated-test/ ../test/uk/gov/hmrc/fhregistrationfrontend
 rm -rf ../generated-test/
 
 echo ""
 echo "Moving integration test files from generated-it-test/ to it/"
 echo ""
 
-rsync -avm --include='*.scala' -f 'hide,! */' ../generated-it-test/ ../it/uk.gov.hmrc.fhregistrationfrontend
+rsync -avm --include='*.scala' -f 'hide,! */' ../generated-it-test/ ../it/uk/gov/hmrc/fhregistrationfrontend
 rm -rf ../generated-it-test/
 
 echo ""
