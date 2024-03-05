099a7c4d34ceb3a10cfb6c48ce28dd20a7b5157a
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-03-05 16:21:40 +0000

    DLS-9181: [FHDDS] Nested fieldsets in conditional reveal (EORI) [WCAG] - Initial splitting to three pages WIP

---
+++ app/uk/gov/hmrc/fhregistrationfrontend/forms/journey/ImportingActivitiesPage.scala
@@ -0,0 +1,131 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.fhregistrationfrontend.forms.journey
+
+import play.api.i18n.Messages
+import play.api.libs.json.Format
+import play.api.mvc.Request
+import play.twirl.api.Html
+import uk.gov.hmrc.fhregistrationfrontend.config.AppConfig
+import uk.gov.hmrc.fhregistrationfrontend.forms.models.{Address, EoriNumber, ImportingActivities, ListWithTrackedChanges, StoragePremise}
+import uk.gov.hmrc.fhregistrationfrontend.forms.navigation.Navigation
+import uk.gov.hmrc.fhregistrationfrontend.models.businessregistration.BusinessRegistrationDetails
+
+case class ImportingActivitiesPage(
+  mainPage: Page[Boolean],
+  eoriNumberPage: Page[EoriNumber],
+  section: Option[String] = None
+) extends Page[ImportingActivities] {
+
+  override val id: String = "importingActivities"
+  override val format: Format[ImportingActivities] = ImportingActivities.format
+
+  val mainSection = Some("any")
+
+  override def withData(data: ImportingActivities): Page[ImportingActivities] = {
+    val newSection = if (data.hasEori) section else None
+    this copy (
+      section = newSection,
+      mainPage = mainPage withData data.hasEori,
+//      eoriNumberPage = if (data.hasEori) eoriNumberPage withData data.eoriNumber.get else eoriNumberPage
+      eoriNumberPage = eoriNumberPage withData data.eoriNumber.getOrElse(EoriNumber("", false))
+    )
+  }
+
+  override def parseFromRequest[X](withErrors: Rendering => X, withData: Page[ImportingActivities] => X)(
+    implicit r: Request[_]): X =
+    if (isMainSection) {
+      mainPage.parseFromRequest(
+        withErrors,
+        mp => {
+          val newValue = this copy (mainPage = mp)
+          withData(newValue)
+        }
+      )
+    } else {
+      eoriNumberPage.parseFromRequest(
+        withErrors,
+        spp => {
+          val newValue = this copy (eoriNumberPage = spp)
+          withData(newValue)
+        }
+      )
+    }
+
+  override val withSubsection: PartialFunction[Option[String], Page[ImportingActivities]] = {
+    case None          => this copy (section = mainSection)
+    case `mainSection` => this copy (section = mainSection)
+    case newSection if hasEori =>
+      this copy (section = newSection,
+      eoriNumberPage = eoriNumberPage)
+  }
+
+  override def nextSubsection: Option[String] =
+    if (isMainSection && hasEori)
+      Some("eoriNumber")
+    else if (isMainSection && !hasEori)
+      None
+    else
+      eoriNumberPage.nextSubsection
+
+  override def previousSubsection: Option[String] =
+    if (isMainSection) None
+    else eoriNumberPage.previousSubsection orElse mainSection
+
+  private def isMainSection = section.isEmpty || (section == mainSection)
+  private def hasEori = mainPage.data contains true
+
+  override def render(
+    bpr: BusinessRegistrationDetails,
+    navigation: Navigation)(implicit request: Request[_], messages: Messages, appConfig: AppConfig): Html =
+    if (isMainSection)
+      mainPage.render(bpr, navigation)
+    else
+      eoriNumberPage.render(bpr, navigation)
+
+  override val data: Option[ImportingActivities] = {
+    mainPage.data map { hasEori =>
+      ImportingActivities(hasEori, eoriNumberPage.data)
+    }
+  }
+
+  override def delete: Option[Page[ImportingActivities]] =
+    if (isMainSection)
+      None
+    else
+      eoriNumberPage.delete map { updatedStoragePremisePage =>
+        this copy (
+          eoriNumberPage = updatedStoragePremisePage
+        )
+      }
+
+  override def pageStatus: PageStatus =
+    if (mainPage.pageStatus != Completed) mainPage.pageStatus
+    else if (!hasEori) Completed
+    else if (eoriNumberPage.pageStatus == Completed) Completed
+    else InProgress
+
+  override def lastSection: Option[String] =
+    if (hasEori)
+      eoriNumberPage.lastSection
+    else
+      None
+
+  override def updatedAddresses: List[Address] =
+    if (isMainSection) mainPage.updatedAddresses
+    else eoriNumberPage.updatedAddresses
+}
