f56301974ad42f57f1ff0983f94a572ad66bb555
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-04-02 13:39:32 +0100

    DLS-9181: Enter Eori Number lable h1 large size

--- app/uk/gov/hmrc/fhregistrationfrontend/forms/models/ImportingActivities.scala
+++ app/uk/gov/hmrc/fhregistrationfrontend/forms/models/ImportingActivities.scala
@@ -21,7 +21,6 @@ import play.api.libs.json.Json
 case class ImportingActivities(
   hasEori: Boolean,
   eoriNumber: Option[EoriNumber] = None,
-// TODO: May want to remove default params from below if possible
   eori: Option[String] = None,
   goodsImported: Option[Boolean] = None
 )
