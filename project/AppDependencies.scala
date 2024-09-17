46b365afbf371550afb15d80078684eeebd1ef05
Drew Miley <drew.miley@testdrivensolutions.co.uk>
2024-09-17 14:55:18 +0100

    DLS-10268: Upgraded dependencies and plugins

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,22 +4,22 @@ import sbt._
 object AppDependencies {
 
   val playVersion = "play-30"
-  val bootstrapVersion = "8.4.0"
-  val hmrcMongoVersion = "1.7.0"
+  val bootstrapVersion = "8.6.0"
+  val hmrcMongoVersion = "2.2.0"
   val monocleVersion = "2.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
     "uk.gov.hmrc"                %% s"bootstrap-frontend-$playVersion"            % bootstrapVersion,
     "uk.gov.hmrc"                %% s"play-frontend-hmrc-$playVersion"            % "8.5.0",
-    "uk.gov.hmrc"                %% s"play-partials-$playVersion"                 % "9.1.0",
+    "uk.gov.hmrc"                %% s"play-partials-$playVersion"                 % "10.0.0",
     "uk.gov.hmrc"                %% s"play-hmrc-api-$playVersion"                 % "8.0.0",
-    "uk.gov.hmrc"                %% s"http-caching-client-$playVersion"           % "11.2.0",
-    "uk.gov.hmrc"                %% s"play-conditional-form-mapping-$playVersion" % "2.0.0",
+    "uk.gov.hmrc"                %% s"http-caching-client-$playVersion"           % "12.1.0",
+    "uk.gov.hmrc"                %% s"play-conditional-form-mapping-$playVersion" % "3.2.0",
     "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-$playVersion"                    % hmrcMongoVersion,
-    "org.typelevel"              %% "cats-core"                                   % "2.9.0",
-    "org.typelevel"              %% "cats-kernel"                                 % "2.9.0",
-    "com.typesafe.play"          %% "play-json"                                   % "2.9.4",
+    "org.typelevel"              %% "cats-core"                                   % "2.12.0",
+    "org.typelevel"              %% "cats-kernel"                                 % "2.12.0",
+    "com.typesafe.play"          %% "play-json"                                   % "2.10.6",
     "com.github.tototoshi"       %% "play-json-naming"                            % "1.5.0",
     "org.julienrf"               %% "play-json-derived-codecs"                    % "10.1.0",
     "com.github.julien-truffaut" %% "monocle-core"                                % monocleVersion,
@@ -33,7 +33,7 @@ object AppDependencies {
     "uk.gov.hmrc"                %% s"bootstrap-test-$playVersion"  % bootstrapVersion,
     "uk.gov.hmrc.mongo"          %% s"hmrc-mongo-test-$playVersion" % hmrcMongoVersion,
     "org.pegdown"                %  "pegdown"                       % "1.6.0",
-    "org.jsoup"                  %  "jsoup"                         % "1.15.4",
+    "org.jsoup"                  %  "jsoup"                         % "1.18.1",
     "com.github.julien-truffaut" %% "monocle-law"                   % monocleVersion,
     "com.github.java-json-tools" %  "json-schema-validator"         % "2.2.14"
   ).map(_ % "test, it")
